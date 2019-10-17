ad9ab57c2f76bdae8c1802e7d0273f496533e127
System Administrator <root@Graysons-MacBook-Pro.local>
2019-10-17 18:13:38 +0300

    Added deteting method on both delete and products Item components

--- src/components/DeleteProductsItems.vue
+++ src/components/DeleteProductsItems.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <h1>PRODUCTS</h1>
+      <h1>Posts</h1>
         <div class="row">
           <div class="col-md-10"></div>
           <div class="col-md-2">
@@ -11,7 +11,7 @@
         <table class="table table-hover">
             <thead>
             <tr>
-                <th>Product Name</th> 
+                <th>Product Name</th>
                 <th>Category</th>
                 <th>Number of Proucts on Stock</th>
                 <th>Actions</th>
@@ -23,7 +23,7 @@
                     <td>{{ item.category }}</td>
                     <td>{{ item.number_of_products }}</td>
                     <td><router-link :to="{name: 'edit', params: { id: item.id }}" class="btn btn-primary">Edit</router-link></td>
-                    <td><button class="btn btn-danger">Delete</button></td>
+                    <td><button class="btn btn-danger" @click.prevent="deletePost(item.id)">Delete</button></td>
                 </tr>
             </tbody>
         </table>
@@ -38,10 +38,19 @@
         }
       },
       created() {
-      let uri = 'http://127.0.0.1:8000/api/productsItems';
+      let uri = 'http://vuelaravelcrud.test/api/posts';
       this.axios.get(uri).then(response => {
         this.item = response.data.data;
       });
+    },
+    methods: {
+      deletePost(id)
+      {
+        let uri = `http://vuelaravelcrud.test/api/post/delete/${id}`;
+        this.axios.delete(uri).then(response => {
+          this.item.splice(this.items.indexOf(id), 1);
+        });
+      }
     }
   }
 </script>
