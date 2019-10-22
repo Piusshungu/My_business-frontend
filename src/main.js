d2d6ccf9614c1cfe686bd154ea4e87548b1b9f6e
System Administrator <root@Graysons-MacBook-Pro.local>
2019-10-22 09:23:05 +0300

    Added vuetify files configurations

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Vuetify from 'vuetify'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
+
+Vue.use(Vuetify,{
+  theme: {
+    primary: "red"
+  }
+  
+});
 
 Vue.config.productionTip = false;
+new Vue({
+  vuetify,
+}).$mount('#app')
 
 new Vue({
   router,
